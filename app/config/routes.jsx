db452e1354424fcead7078189069e816ed573912
David Tao <ruoyutao@gmail.com>
2016-06-21 12:11:30 +0800

    Add app config and routes file

---
+++ app/config/routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../components/main.jsx'
+import Home from '../components/home.jsx'
+import {Router, Route, Link, IndexRoute} from 'react-router'
+
+const routes = () => 
+	<Route path="/" component={Main}>
+		<IndexRoute component={Home} />
+	</Route>
+
+
+export default routes()
