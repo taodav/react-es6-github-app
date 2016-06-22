55d182ea924dbed7b7eeca77a61584b2b0d8ee69
David Tao <ruoyutao@gmail.com>
2016-06-23 06:09:25 +0800

    Push firebase init to main file

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SearchGithub from './SearchGithub.jsx'
+import firebase from 'firebase'
 
 class Main extends React.Component {
+  constructor(){
+    super();
+    var config = {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+    };
+    firebase.initializeApp(config);
+  }
 	render(){
 		return (
 			<div className="main-container">
