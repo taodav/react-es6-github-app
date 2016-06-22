af0965b73ae65701849ff0441a3e471d06d9d227
David Tao <ruoyutao@gmail.com>
2016-06-23 06:08:10 +0800

    Add list for all user data

--- app/components/Github/userprofile.jsx
+++ app/components/Github/userprofile.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 class UserProfiles extends React.Component {
 	render(){
 		return <div>
+	        {this.props.bio.login && <a href={this.props.bio.html_url}><h3>{this.props.bio.login}</h3></a>}
 			{this.props.bio.avatar_url && <li className="list-group-item"> <img src={this.props.bio.avatar_url} className="img-rounded img-responsive"/></li>}
 	        {this.props.bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
-	        {this.props.bio.login && <li className="list-group-item">Username: {this.props.bio.login}</li>}
 	        {this.props.bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
 	        {this.props.bio.location && <li className="list-group-item">Location: {this.props.bio.location}</li>}
 	        {this.props.bio.company && <li className="list-group-item">Company: {this.props.bio.company}</li>}
