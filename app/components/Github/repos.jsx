b05a15f15876285f296c412d6e66b23375f1b11d
David Tao <ruoyutao@gmail.com>
2016-06-23 05:42:22 +0800

    Add list of repos from github

--- app/components/Github/repos.jsx
+++ app/components/Github/repos.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 class Repos extends React.Component {
 	render(){
+		let repos = this.props.repos.map(function(repo, index){
+			return <li className="list-group-item" key={index}>
+					{repo.html_url && <a href={repo.html_url}>{repo.full_name}</a>}
+					{repo.description && <p>{repo.description}</p>}
+				</li>
+		})
 		return <div>
-			<p>Repos: {this.props.repo}</p>
+			<h3>User Repos</h3>
+			<ul className="list-group">
+				{repos}
+			</ul>
 		</div>
 	}
 }
