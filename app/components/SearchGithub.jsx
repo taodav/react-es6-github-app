4a8fb65ab1fcc8100749c515729e8c513e7c59d6
David Tao <ruoyutao@gmail.com>
2016-06-23 05:44:57 +0800

    Add handleSubmit function to link to another route

---
+++ app/components/SearchGithub.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {hashHistory} from 'react-router';
+
+class SearchGithub extends React.Component {
+	constructor(){
+		super();
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.getRef = this.getRef.bind(this)
+	}
+
+	handleSubmit(){
+		let username = this.usernameRef.value;
+		this.usernameRef.value = '';
+		hashHistory.push('profile/' + username)
+	}
+
+	getRef(ref){
+		this.usernameRef = ref
+	}
+
+	render(){
+		return <div className="col-sm-12">
+				<form onSubmit={this.handleSubmit}>
+					<div className="form-group col-sm-7">
+						<input type="text" className="form-control" ref={this.getRef} />
+					</div>
+					<div className="form-group col-sm-5">
+						<button type="submit" className="btn btn-block btn-primary">Search Github</button>
+					</div>
+				</form>
+			</div>
+	}
+};
+
+
+export default SearchGithub;
