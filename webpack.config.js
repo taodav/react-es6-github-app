2fee6bc8785224e66f0509e7f7b481641e2cbbff
David Tao <ruoyutao@gmail.com>
2016-06-22 18:48:37 +0800

    Add updated webpack config file to include default plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,17 @@
+require('dotenv').config()
+var webpack = require('webpack')
+
 module.exports = {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET)
+      }
+})
+  ],
   entry: "./app/App.jsx",
   output: {
     filename: "public/bundle.js"
